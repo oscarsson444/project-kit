1cef9ec6f64fbc6af4e3629bf4176939a0b2ce98
oscarsson444 <oscarsson444@gmail.com>
2023-12-18 15:58:44 +0100

    feat: Added quotes, tts and voice recognition.

---
+++ public/data/quotes.ts
@@ -0,0 +1,203 @@
+export const quotes = {
+  "1": {
+    quote: "A rose by any other name would smell as sweet.",
+    author: "William Shakespeare",
+    language: "English",
+  },
+  "2": {
+    quote: "All that glitters is not gold.",
+    author: "William Shakespeare",
+    language: "English",
+  },
+  "3": {
+    quote: "All the world’s a stage, and all the men and women merely players.",
+    author: "William Shakespeare",
+    language: "English",
+  },
+  "4": {
+    quote:
+      "Ask not what your country can do for you; ask what you can do for your country.",
+    author: "John Kennedy",
+    language: "English",
+  },
+  "5": {
+    quote: "Ask, and it shall be given you; seek, and you shall find.",
+    author: "the Bible",
+    language: "Greek",
+  },
+  "6": {
+    quote: "Eighty percent of success is showing up.",
+    author: "Woody Allen",
+    language: "English",
+  },
+  "7": {
+    quote: "Elementary, my dear Watson.",
+    author: "Sherlock Holmes (character)",
+    language: "English",
+  },
+  "8": {
+    quote: "For those to whom much is given, much is required.",
+    author: "the Bible",
+    language: "Greek",
+  },
+  "9": {
+    quote: "Frankly, my dear, I don't give a damn.",
+    author: "Rhett Butler (character)",
+    language: "English",
+  },
+  "10": {
+    quote:
+      "Genius is one percent inspiration and ninety-nine percent perspiration.",
+    author: "Thomas Edison",
+    language: "English",
+  },
+  "11": {
+    quote: "Go ahead, make my day.",
+    author: "Harry Callahan (character)",
+    language: "English",
+  },
+  "12": {
+    quote: "He travels the fastest who travels alone.",
+    author: "Rudyard Kipling",
+    language: "English",
+  },
+  "13": {
+    quote: "Hell has no fury like a woman scorned.",
+    author: "William Congreve",
+    language: "English",
+  },
+  "14": {
+    quote: "Hell is other people.",
+    author: "Jean-Paul Sartre",
+    language: "French",
+  },
+  "15": {
+    quote: "Here's looking at you, kid.",
+    author: "Rick Blaine (character)",
+    language: "English",
+  },
+  "16": {
+    quote: "Houston, we have a problem.",
+    author: "Jim Lovell (character)",
+    language: "English",
+  },
+  "17": {
+    quote:
+      "I have a dream that my four little children will one day live in a nation where they will not be judged by the color of their skin but by the content of their character.",
+    author: "Martin Luther King",
+    language: "English",
+  },
+  "18": {
+    quote: "I have always depended on the kindness of strangers.",
+    author: "Blanche Dubois (character)",
+    language: "English",
+  },
+  "19": {
+    quote: "I love the smell of napalm in the morning.",
+    author: "Lt. Kilgore (character)",
+    language: "English",
+  },
+  "20": {
+    quote: "I think therefore I am.",
+    author: "Rene Descartes",
+    language: "French",
+  },
+  "21": {
+    quote: "If at first you don’t succeed, try, try again.",
+    author: "W. E. Hickson",
+    language: "English",
+  },
+  "22": {
+    quote: "If you are going through hell, keep going.",
+    author: "Winston Churchill",
+    language: "English",
+  },
+  "23": {
+    quote: "If you build it, they will come.",
+    author: "Joe Jackson (character)",
+    language: "English",
+  },
+  "24": {
+    quote: "If you want something done right, do it yourself.",
+    author: "Charles-Guillaume Étienne",
+    language: "French",
+  },
+  "25": {
+    quote:
+      "If you want something said, ask a man; if you want something done, ask a woman.",
+    author: "Margaret Thatcher",
+    language: "English",
+  },
+  "26": {
+    quote: "I'll be back.",
+    author: "Terminator (character)",
+    language: "English",
+  },
+  "27": {
+    quote: "I'm gonna make him an offer he can't refuse.",
+    author: "Vito Corleone (character)",
+    language: "English",
+  },
+  "28": {
+    quote: "I've got a feeling we're not in Kansas anymore.",
+    author: "Dorothy (character)",
+    language: "English",
+  },
+  "29": {
+    quote: "Keep your friends close, but your enemies closer.",
+    author: "Michael Corleone (character)",
+    language: "English",
+  },
+  "30": {
+    quote: "Knowledge is power.",
+    author: "Sir Francis Bacon",
+    language: "Latin",
+  },
+  "31": {
+    quote:
+      "Life is like a box of chocolates. You never know what you’re gonna get.",
+    author: "Forrest Gump (character)",
+    language: "English",
+  },
+  "32": {
+    quote:
+      "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+    author: "Albert Einstein",
+    language: "English",
+  },
+  "33": {
+    quote: "May the Force be with you.",
+    author: "Star Wars (many characters)",
+    language: "English",
+  },
+  "34": {
+    quote: "No one can make you feel inferior without your consent.",
+    author: "Eleanor Roosevelt",
+    language: "English",
+  },
+  "35": {
+    quote: "Not all those who wander are lost.",
+    author: "J. R. R. Tolkein",
+    language: "English",
+  },
+  "36": {
+    quote: "Nothing is certain except for death and taxes.",
+    author: "Benjamin Franklin",
+    language: "English",
+  },
+  "37": {
+    quote: "Parting is such sweet sorrow",
+    author: "William Shakespeare",
+    language: "English",
+  },
+  "38": {
+    quote: "Power corrupts; absolute power corrupts absolutely.",
+    author: "John Dalberg-Acton",
+    language: "English",
+  },
+  "39": {
+    quote: "Speak softly and carry a big stick",
+    author: "Theodore Roosevelt",
+    language: "English",
+  },
+};
